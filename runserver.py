cb0822c32d6635694573790c444017ffd9a37d13
thomsart <psychoid@hotmail.fr>
2021-11-29 17:29:00 +0100

    mise en place de app()

---
+++ runserver.py
@@ -0,0 +1,35 @@
+""" 
+This module is use to execute the server of the site www.thomsart.tech """
+
+import os
+
+from bottle import Bottle, route, static_file, template, run, get
+
+app = Bottle()
+
+
+@app.route('<filename:path>')
+def return_css(filename):
+    """ The path to return css files """
+
+    if str(filename) == "/base.css":
+        return static_file(filename, root='static/css/')
+    else:
+        return static_file(filename, root='static/assets/img/')
+
+
+@app.route('/')
+def home():
+    """ The path to return the home template """
+
+    return template('home', name=home)
+
+
+@app.route('/mentions_legales')
+def mentions_legales():
+    """ The path to return the mentions legales template """
+
+    return template('mentions_legales', name=mentions_legales)
+
+
+run(app, reloader=True, host='localhost', port=8080, debug=True)
