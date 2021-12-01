e1fe3891e7de67a281fa0f0007afce43dfa2df0e
thomsart <psychoid@hotmail.fr>
2021-12-01 11:14:53 +0100

    import modif

--- launch_cv.py
+++ launch_cv.py
@@ -5,6 +5,7 @@ This module launch the cv on ubuntu vps """
 
 print('launch on vps')
 
-from launch_cv_localy import *
+from launch_cv_localy import app
+from bottle import run
 
 run(app, host='92.222.167.113', port=80, debug=None)
